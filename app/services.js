20c9ecd287a52d32ad467815684cb6b3cfe38873
Brian de Alwis <bsd@mt.ca>
2014-11-13 20:49:50 -0500

    Refactor and rework

--- app/services.js
+++ app/services.js
@@ -2,65 +2,105 @@ angular.module('ight', [])
 .constant('_', window._)
 .constant('InstagramAppId', 'f05f03002ba04cb48c65c7ddae75def1')
 
+.directive('igCompact', function($compile) {
+	return {
+		restrict: 'E',
+		scope: { post: '=content' },
+		template: 
+			'<div class="ig-compact">'
+			+ '<a href="{{post.link}}">' 
+			+ '<img src="{{post.images.thumbnail.url}}">'
+			+ '</a>'
+			+ '<div class="date">{{post.created_time * 1000 | date:medium}}</div>'
+			+ '<div class="user"><a ng-href="http://instagram.com/{{post.user.username}}">{{post.user.username}}</a></div>'
+			+ '<div class="caption">{{post.caption.text}}</div>'
+			+ '</div>'
+	};
+})
+
 .service('InstagramTags', function($http, $q, InstagramAppId, _) {
-	var initialTag;
-	var minTagId = -1;
-	var maxTagId = -1;
-	
-	// the results
-	var userList;
-	var hashTags;
-	var userByTags;
-	
 	var _ = window._;
 	
-	function processMedia(media) {
-		console.log("processMedia: " + media.length);
-		var countByUsers = _.countBy(media, function(m) { return m.user.username + "[" + m.user.id + "]"; });
+	var InstagramTagSummary = function(tag) {
+		this.tag = tag;
+		
+		this.untilDate = undefined;
+		this.maxPosts = undefined;
+		
+		this.minTagId = undefined;
+		this.maxTagId = undefined;
+		this.media = [];
+	};	
+	
+	function summarize(media) {
+		console.log("summarize: " + media.length);
+		media = _.sortBy(media, function(m) { return m.created_time * 1; });
+		var countByUsers = _.countBy(media, function(m) { return m.user.username; });
 		var countByHashtags = _.chain(media).pluck('tags').flatten().countBy(_.identity).value();
 		var countHashtagsByUser = _.chain(media).map(function(m) {
-				return _.map(m.tags, function(t) { return m.user.id + "[" + m.user.username + "]:" + t; });
+				return _.map(m.tags, function(t) { return m.user.username + ":" + t; });
 			}).flatten().countBy(_.identity).value();
-		// FIXME: SHOULD try to figure out how to combine these results with existing results
+		// FIXME: retain and combine these results with existing results?
 		return { 
+			earliest: _.first(media),
+			latest: _.last(media),
 			byUsers: countByUsers, 
 			byHashtags: countByHashtags, 
 			hashtagsByUser: countHashtagsByUser
 		};
 	}
 	
+	InstagramTagSummary.prototype.update = function(filter) {
+		var deferred = $q.defer();
+		this.processMoreTags()
+		.then(function success(media) {
+			deferred.resolve(summarize(filter ? _.filter(media, filter) : media));
+		});
+		return deferred.promise;
+	};
+	
 	/**
 	 * @return promise with the media
 	 */
-	function processTagUntil(tag, untilDate, maximum, media, minTagId) {
+	InstagramTagSummary.prototype.processMoreTags = function() {
+		var self = this;
 		// Instagram tagIds are in microseconds
-		if(!minTagId
-				|| (untilDate && minTagId > 0 && (minTagId / 1000) < untilDate.getTime())
-				|| (maximum > 0 && media.length > maximum)) {
-			return $q.when(media);
+		if((this.untilDate && this.minTagId > 0 && (this.minTagId / 1000) < this.untilDate.getTime())
+				|| (this.maxPosts > 0 && this.media.length > this.maxPosts)) {
+			console.log(this.tag + ": curtailing query with " + this.media.length + " posts total");	
+			return $q.when(this.media);
 		}
-		console.log("about to fetch more tags: min_tag_id=" + minTagId);
-		return $http.jsonp('https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=JSON_CALLBACK', {
-			headers: {"Accept":undefined},
-			params: {
-				//callback: '?',
-				client_id: InstagramAppId,
-				min_tag_id: (minTagId > 0 ? minTagId : undefined),
-				count: (maximum > 0 ? (maximum - media.length).toString() : undefined)
-			}}).then(
+		var params = { client_id: InstagramAppId };
+		if(this.minTagId > 0) { params.max_tag_id = this.minTagId; }
+		if(this.maxPosts > 0) { params.count = (this.maxPosts - this.media.length).toString(); }
+		return $http.jsonp('https://api.instagram.com/v1/tags/' + this.tag + '/media/recent?callback=JSON_CALLBACK', {
+				headers: {"Accept":undefined},
+				params: params
+			}).then(
 				function success(response) {
-					return processTagUntil(tag, untilDate, maximum,
-						response.data.data.concat(media), response.data.pagination.min_tag_id);
+					self.media = response.data.data.concat(self.media); 
+					if(response.data.pagination && (!self.minTagId || response.data.pagination.next_max_tag_id < self.minTagId)) {
+						self.minTagId = response.data.pagination.next_max_tag_id;
+						return self.processMoreTags();
+					} else {
+						console.log(self.tag + ": no more posts; " + self.media.length + " posts total");
+						console.dir(response);
+						return $q.when(self.media);
+					}
 				},
 				function error(err) {
 					// return what we've got so far
 					console.log("error fetching data: " + JSON.stringify(err));
-					return media;
+					return self.media;
 				});	
 	}
 	
 	
 	return {
+		summarizeTag: function(tag) {
+			return new InstagramTagSummary(tag);
+		},
+	
 		/**
 		 * Resolve the provided handle to the corresponding Instagram id,
 		 * a stable numeric value good for the life of the account. 
@@ -81,7 +121,7 @@ angular.module('ight', [])
 						count: 1
 					}}).then(
 						function success(response) {
-							response.resolve(response.data.length == 0 ?
+							result.resolve(response.data.length == 0 ?
 									undefined : response.data[0].id);
 						},
 						function error(err) {
@@ -89,29 +129,20 @@ angular.module('ight', [])
 							result.reject(err);
 						});
 				return result.promise;
-		},
-
-		processUser: function(handle, date, maximum) {
-			$http.get('https://api.instagram.com/v1/users/' + igUserId + '/media/recent',
-				{
-							headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-							body: {
-								callback: '?',
-								client_id: InstagramAppId,
-								q: req.params.user,
-								count: 1
-							}}).then(
-								function success(response) {},
-								function error(err) {});	
-		},
-
-		summarizeTag: function(tag, date, maximum) {
-			var deferred = $q.defer();
-			processTagUntil(tag, date, maximum, [], -1)
-			.then(function success(media) {
-				deferred.resolve(processMedia(media));
-			});
-			return deferred.promise;
 		}
+
+//		processUser: function(handle, date, maximum) {
+//			$http.get('https://api.instagram.com/v1/users/' + igUserId + '/media/recent',
+//				{
+//							headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+//							body: {
+//								callback: '?',
+//								client_id: InstagramAppId,
+//								q: req.params.user,
+//								count: 1
+//							}}).then(
+//								function success(response) {},
+//								function error(err) {});	
+//		}
 	};
 });
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,18 +11,71 @@ angular.module('myApp.view1', ['ngRoute', 'ight'])
 
 .controller('View1Ctrl', function($scope, InstagramTags) {
 	console.log("about to invoke summarizer");
+	$scope.hashtag = null;
+	$scope.maxPosts = 20;
+	$scope.untilDate = null;
+	$scope.pluck = 100;
+	var summary;
+	
 	function pluckTop(bag, count) {
 		var _ = window._;
 		return _.chain(bag).pairs().sortBy(function(p) { return -p[1]; }).first(count).value();
 	}
 	
-	InstagramTags.summarizeTag('stelladot', null, 30).then(
-		function success(results) {
-			$scope.results = {
-					byUsers: pluckTop(results.byUsers, 10), 
-					byHashtags: pluckTop(results.byHashtags, 10), 
-					hashtagsByUser: pluckTop(results.hashtagsByUser, 10)
-			};
-			console.dir($scope.results);
-		});
+	function update() {
+		if(summary) {
+			summary.update().then(
+				function success(results) {
+					$scope.results = {
+							media: summary.media,
+							earliest: results.earliest,
+							latest: results.latest,
+							byUsers: pluckTop(results.byUsers, $scope.pluck), 
+							byHashtags: pluckTop(results.byHashtags, $scope.pluck), 
+							hashtagsByUser: pluckTop(results.hashtagsByUser, $scope.pluck)
+					};
+					console.dir($scope.results);
+				});
+		} else {
+			$scope.results = null;
+		}
+	}
+
+	$scope.extractUsername = function(usertag) {
+		return usertag.split(':')[0];
+	};
+	
+	$scope.showPostsFromUser = function(username) {
+		$scope.highlightText = "Posts from " + username;
+		$scope.highlightedPosts = _.filter(summary.media,
+			function(m) { return m.user.username == username; });
+	};
+	$scope.showPostsWithTag = function(tag) {
+		$scope.highlightText = 'Posts with #' + $scope.hashtag + " #" + tag;
+		$scope.highlightedPosts = _.filter(summary.media,
+			function(m) { return _.contains(m.tags, tag); });
+	};
+	$scope.showPostsFromUserWithTag = function(userTag) {
+		var username = userTag.split(':')[0];
+		var tag = userTag.split(':')[1];
+		$scope.highlightText = 'From ' + username + " with #" + tag;
+		$scope.highlightedPosts = _.filter(summary.media,
+			function(m) { return m.user.username == username
+				&& _.contains(m.tags, tag); });
+	};
+	
+	$scope.run = function() {
+		$scope.results = null;
+
+		if(!$scope.hashtag) {
+			summary = null;
+		} else {
+			if(!summary || summary.tag != $scope.hashtag) {
+				summary = InstagramTags.summarizeTag($scope.hashtag);
+			}
+			summary.maxPosts = $scope.maxPosts;
+			summary.untilDate = $scope.untilDate;
+		}
+		update();
+	};
 });
